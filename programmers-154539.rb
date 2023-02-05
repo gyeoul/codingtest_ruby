e9e6745b69580c9e0b913c3b6da7016e07959173
gyeoul <clsmay.1st@gmail.com>
2023-02-05 17:15:49 +0900

    Solved 154539

--- programmers-154539.rb
+++ programmers-154539.rb
@@ -5,12 +5,33 @@ numbers2 = [9, 1, 5, 3, 6, 2]	#[-1, 5, 6, 6, -1, -1]
 numbers = numbers2
 
 answer = []
+=begin
 def solution(numbers)
   answer = []
+  n = numbers.length
+  hash_table = {}
 
+  # Create a hash table to store the indexes of each number in the array
   numbers.each_with_index do |v,i|
-    answer.push(nextnum(i,numbers,v))
+    hash_table[v] = i
   end
+
+  # Sort the numbers in ascending order
+  numbers.sort!
+
+  # Find the next greater number for each value
+  numbers.each_with_index do |v,i|
+    j = i + 1
+    while j < n && numbers[j] <= v
+      j += 1
+    end
+    if j < n
+      answer[hash_table[v]] = numbers[j]
+    else
+      answer[hash_table[v]] = -1
+    end
+  end
+
   return answer
 end
 
@@ -19,6 +40,22 @@ def nextnum(i,arr,v)
     return arr[j] if v<arr[j]
   end
   -1
+end
+=end
+
+def solution(numbers)
+  answer = [-1] * numbers.length
+  stack = []
+
+  numbers.each_with_index do |v, i|
+    while stack.any? && numbers[stack.last] < v
+      j = stack.pop
+      answer[j] = v
+    end
+    stack.push(i)
+  end
+
+  return answer
 end
 
 print solution(numbers)
